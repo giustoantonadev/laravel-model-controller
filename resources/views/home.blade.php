d584f80b59565ba7c1e5ce48b22dc11dd0fa7cd1
giustoantonadev <giustoantonadev@gmail.com>
2026-07-17 13:18:52 +0200

    feat: enhance movie card with overlay and play icon, and improve layout structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,30 +6,45 @@
 
     <div class="row g-4">
         @foreach ($movies as $movie)
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
 
-                <div class="movie-card h-100">
+            <div class="movie-card h-100">
+
+                {{-- Poster + overlay + play --}}
+                <div class="poster-wrapper">
 
                     {{-- Poster TMDB --}}
-                    <div class="poster-wrapper">
-                        <img 
-                            src="{{ $movie->image }}" 
-                            class="poster-img"
-                            alt="{{ $movie->original_title }}"
-                        >
+                    <img
+                        src="{{ $movie->image }}"
+                        class="poster-img"
+                        alt="{{ $movie->original_title }}">
+
+                    {{-- Overlay blu Prime Video --}}
+                    <div class="poster-overlay"></div>
+
+                    {{-- Icona Play --}}
+                    <div class="play-icon">
+                        ▶
                     </div>
 
-                    {{-- Info --}}
-                    <div class="movie-info p-3">
-                        <h5 class="text-white mb-1">{{ $movie->original_title }}</h5>
-                        <p class="text-secondary mb-1">{{ $movie->nationality }}</p>
-                        <p class="text-secondary mb-1">{{ $movie->date }}</p>
-                        <p class="text-warning fw-bold">⭐ {{ $movie->vote }}</p>
+                    {{-- Badge voto --}}
+                    <div class="rating-badge">
+                        ⭐ {{ $movie->vote }}
                     </div>
 
                 </div>
 
+                {{-- Info --}}
+                <div class="movie-info p-3">
+                    <h5 class="text-white mb-1">{{ $movie->original_title }}</h5>
+                    <p class="text-secondary mb-1">{{ $movie->nationality }}</p>
+                    <p class="text-secondary mb-1">{{ $movie->date }}</p>
+                </div>
+
             </div>
+
+        </div>
+
         @endforeach
     </div>
 </div>
